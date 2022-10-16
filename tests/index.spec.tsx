3fd05a1846285ec199be77a85c4f48a3d2b01d12
Nathan Hoad <nathan@nathanhoad.net>
2022-10-16 16:16:19 +1000

    Update to React 18
    Closes #22

--- tests/index.spec.tsx
+++ tests/index.spec.tsx
@@ -1,82 +1,70 @@
-import { Expect, Test, TestCase } from "alsatian";
-import * as React from "react";
-import * as TestRenderer from "react-test-renderer";
+import React from "react";
+import { render } from "@testing-library/react"
+import "@testing-library/jest-dom";
 
 import Timestamp from "../src";
 
-export class TestTimestamp {
-  @Test("Can render a timestamp with defaults")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "26 Mar 2019, 10:30am")
-  @TestCase(new Date(2019, 2, 26, 16, 30), "26 Mar 2019, 4:30pm")
-  public testDefaults(date: Date, expectedOutput: string) {
-    const { root } = TestRenderer.create(<Timestamp date={date} />);
-    Expect(root.findByType("time").children).toEqual([expectedOutput]);
-  }
+describe("<Timestamp />", () => {
+  it("renders a timestamp with defaults", () => {
+    expect.hasAssertions();
 
-  @Test("Can render a timestamp with the day included")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "Tuesday, 26 Mar 2019, 10:30am")
-  @TestCase(new Date(2019, 2, 27, 16, 30), "Wednesday, 27 Mar 2019, 4:30pm")
-  public testWithDay(date: Date, expectedOutput: string) {
-    const { root } = TestRenderer.create(
-      <Timestamp date={date} options={{ includeDay: true }} />
-    );
-    Expect(root.findByType("time").children).toEqual([expectedOutput]);
-  }
+    const { container, rerender } = render(<Timestamp date={new Date(2019, 2, 26, 10, 30)} />);
+    expect(container).toHaveTextContent("26 Mar 2019, 10:30am");
 
-  @Test("Can render a timestamp in 24 hour")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "26 Mar 2019, 10:30")
-  @TestCase(new Date(2019, 2, 27, 16, 30), "27 Mar 2019, 16:30")
-  public test24Hour(date: Date, expectedOutput: string) {
-    const { root } = TestRenderer.create(
-      <Timestamp date={date} options={{ twentyFourHour: true }} />
-    );
-    Expect(root.findByType("time").children).toEqual([expectedOutput]);
-  }
+    rerender(<Timestamp date={new Date(2019, 2, 26, 16, 30)} />);
+    expect(container).toHaveTextContent("26 Mar 2019, 4:30pm")
+  });
 
-  @Test("Can render a timestamp with just the date")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "26 Mar 2019")
-  @TestCase(new Date(2019, 2, 27, 16, 30), "27 Mar 2019")
-  public testJustDate(date: Date, expectedOutput: string) {
-    const { root } = TestRenderer.create(
-      <Timestamp date={date} options={{ format: "date" }} />
-    );
-    Expect(root.findByType("time").children).toEqual([expectedOutput]);
-  }
+  it("renders with the day included", () => {
+    expect.hasAssertions();
 
-  @Test("Can render a timestamp with just the time")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "10:30am")
-  @TestCase(new Date(2019, 2, 27, 16, 30), "4:30pm")
-  public testJustTime(date: Date, expectedOutput: string) {
-    const { root } = TestRenderer.create(
-      <Timestamp date={date} options={{ format: "time" }} />
-    );
-    Expect(root.findByType("time").children).toEqual([expectedOutput]);
-  }
+    const { container, rerender } = render(<Timestamp date={new Date(2019, 2, 26, 10, 30)} options={{ includeDay: true }} />);
+    expect(container).toHaveTextContent("Tuesday, 26 Mar 2019, 10:30am");
 
-  @Test("Can render a date relative to another date")
-  @TestCase(
-    new Date(2022, 3, 28, 10, 30),
-    new Date(2022, 3, 28, 10, 31),
-    "1 minute"
-  )
-  @TestCase(
-    new Date(2022, 3, 28, 10, 30),
-    new Date(2022, 3, 26, 10, 30),
-    "2 days"
-  )
-  @TestCase(
-    new Date(2022, 3, 28, 10, 30),
-    new Date(2024, 3, 26, 10, 31),
-    "2 years"
-  )
-  public testDistanceBetweenDates(
-    date: Date,
-    relativeTo: Date,
-    expectedOutput: string
-  ) {
-    const { root } = TestRenderer.create(
-      <Timestamp date={date} relativeTo={relativeTo} />
-    );
-    Expect(root.findByType("time").children).toEqual([expectedOutput]);
-  }
-}
+    rerender(<Timestamp date={new Date(2019, 2, 27, 16, 30)} options={{ includeDay: true }} />);
+    expect(container).toHaveTextContent("Wednesday, 27 Mar 2019, 4:30pm");
+  });
+
+  it("renders a time in 24 hour format", () => {
+    expect.hasAssertions();
+
+    const { container, rerender } = render(<Timestamp date={new Date(2019, 2, 26, 10, 30)} options={{ twentyFourHour: true }} />);
+    expect(container).toHaveTextContent("26 Mar 2019, 10:30");
+
+    rerender(<Timestamp date={new Date(2019, 2, 27, 16, 30)} options={{ twentyFourHour: true }} />);
+    expect(container).toHaveTextContent("27 Mar 2019, 16:30");
+  });
+
+  it("renders a time with just the date", () => {
+    expect.hasAssertions();
+
+    const { container, rerender } = render(<Timestamp date={new Date(2019, 2, 26, 10, 30)} options={{ format: "date" }} />);
+    expect(container).toHaveTextContent("26 Mar 2019");
+
+    rerender(<Timestamp date={new Date(2019, 2, 27, 16, 30)} options={{ format: "date" }} />);
+    expect(container).toHaveTextContent("27 Mar 2019");
+  });
+
+  it("renders a time with just the time", () => {
+    expect.hasAssertions();
+
+    const { container, rerender } = render(<Timestamp date={new Date(2019, 2, 26, 10, 30)} options={{ format: "time" }} />);
+    expect(container).toHaveTextContent("10:30am");
+
+    rerender(<Timestamp date={new Date(2019, 2, 27, 16, 30)} options={{ format: "time" }} />);
+    expect(container).toHaveTextContent("4:30pm");
+  });
+
+  it("renders the difference between two dates", () => {
+    expect.hasAssertions();
+
+    const { container, rerender } = render(<Timestamp date={new Date(2019, 2, 26, 10, 30)} relativeTo={new Date(2019, 2, 26, 10, 31)} />);
+    expect(container).toHaveTextContent("1 minute");
+
+    rerender(<Timestamp date={new Date(2019, 2, 28, 10, 30)} relativeTo={new Date(2019, 2, 26, 10, 30)} />);
+    expect(container).toHaveTextContent("2 days");
+
+    rerender(<Timestamp date={new Date(2019, 2, 28, 10, 30)} relativeTo={new Date(2021, 2, 28, 10, 31)} />);
+    expect(container).toHaveTextContent("2 years");
+  });
+});
--- tests/util.spec.ts
+++ tests/util.spec.ts
@@ -1,90 +1,67 @@
-import { Expect, Test, TestCase } from "alsatian";
 import * as Util from "../src/util";
 
-export class TestConvertingToDate {
-  @Test("Can convert numbers to dates")
-  @TestCase(1553560143, 2019)
-  public testNumbers(n: number, expectedYear: number) {
-    Expect(Util.toDate(n) instanceof Date).toBeTruthy();
-    Expect((Util.toDate(n) as Date).getFullYear()).toBe(expectedYear);
-  }
+describe("Util", () => {
+  describe("toDate()", () => {
+    it("converts numbers to dates", () => {
+      expect.hasAssertions();
 
-  @Test("Can convert string to dates")
-  @TestCase("2019-03-26 10:30", 26)
-  @TestCase("1920-03-26 10:30", 26)
-  public testStrings(s: string, expectedDate: number) {
-    Expect(Util.toDate(s) instanceof Date).toBeTruthy();
-    Expect((Util.toDate(s) as Date).getDate()).toBe(expectedDate);
-    Expect(Util.toDate("invalid date")).toBeNull();
-  }
-}
+      expect((Util.toDate(1553560143) as Date).getFullYear()).toBe(2019);
+    });
 
-export class TestFormatDate {
-  @Test("Can format a date with default options")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "26 Mar 2019, 10:30am")
-  public testDefaults(date: Date, expectedOutput: string) {
-    Expect(Util.formatDate(date)).toBe(expectedOutput);
-  }
+    it("converts strings to dates", () => {
+      expect((Util.toDate("2019-03-26 10:30") as Date).getDate()).toBe(26);
+      expect((Util.toDate("1920-03-26 10:30") as Date).getDate()).toBe(26);
 
-  @Test("Can format a date with includeDay option")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "Tuesday, 26 Mar 2019, 10:30am")
-  public testFormatWithDay(date: Date, expectedOutput: string) {
-    Expect(Util.formatDate(date, { includeDay: true })).toBe(expectedOutput);
-  }
+      expect(Util.toDate("invalid date")).toBeNull();
+    })
+  });
 
-  @Test("Can format a date with translation")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "blah, 26 blah 2019, 10:30am")
-  public testFormatWithTranslation(date: Date, expectedOutput: string) {
-    Expect(
-      Util.formatDate(date, {
-        includeDay: true,
-        translate: (key: string) => "blah",
-      })
-    ).toBe(expectedOutput);
-  }
+  describe("formatDate()", () => {
+    it("formats a date with default options", () => {
+      expect.hasAssertions();
 
-  @Test("Can format a date with 24 hour option")
-  @TestCase(new Date(2019, 2, 26, 10, 30), "26 Mar 2019, 10:30")
-  @TestCase(new Date(2019, 2, 26, 12, 0), "26 Mar 2019, 12:00")
-  @TestCase(new Date(2019, 2, 26, 18, 30), "26 Mar 2019, 18:30")
-  @TestCase(new Date(2019, 2, 26, 0, 0), "26 Mar 2019, 0:00")
-  public testFormatWith24Hours(date: Date, expectedOutput: string) {
-    Expect(Util.formatDate(date, { twentyFourHour: true })).toBe(
-      expectedOutput
-    );
-  }
-}
+      expect(Util.formatDate(new Date(2019, 2, 26, 10, 30))).toBe("26 Mar 2019, 10:30am");
+    });
 
-export class TestSecondsBetweenDates {
-  @Test("Can measure the time between two dates")
-  @TestCase(new Date(2018, 2, 26, 10, 30), new Date(2018, 2, 26, 10, 0), -1800)
-  @TestCase(
-    new Date(2000, 2, 26, 10, 30),
-    new Date(2200, 2, 26, 10, 30),
-    6311347200
-  )
-  @TestCase(new Date(2018, 2, 26, 10, 30), new Date(2018, 2, 26, 10, 30), 0)
-  public testCompareWithNowWithDefaults(
-    date: Date,
-    otherDate: Date,
-    expectedOutput: number
-  ) {
-    Expect(Util.secondsBetweenDates(date, otherDate)).toBe(expectedOutput);
-  }
-}
+    it("formats a date with includeDay options", () => {
+      expect.hasAssertions();
 
-export class TestDistanceOfTimeInWords {
-  @Test("Can output relative times in words")
-  @TestCase(1000, "17 minutes")
-  @TestCase(100000000, "3 years")
-  @TestCase(100000000, "3 blah", (key: string) => "3 blah")
-  public testComparingDates(
-    seconds: number,
-    expectedOutput: string,
-    translate?: (key: string) => string
-  ) {
-    Expect(Util.distanceOfTimeInWords(seconds, false, translate)).toBe(
-      expectedOutput
-    );
-  }
-}
+      expect(Util.formatDate(new Date(2019, 2, 26, 10, 30), { includeDay: true })).toBe("Tuesday, 26 Mar 2019, 10:30am");
+    });
+
+    it("formats a date with translations", () => {
+      expect.hasAssertions();
+
+      expect(Util.formatDate(new Date(2019, 2, 26, 10, 30), { includeDay: true, translate: () => "blah", })).toBe("blah, 26 blah 2019, 10:30am");
+    });
+
+    it("formats a date with the 24 hour option", () => {
+      expect.hasAssertions();
+
+      expect(Util.formatDate(new Date(2019, 2, 26, 10, 30), { twentyFourHour: true })).toBe("26 Mar 2019, 10:30");
+      expect(Util.formatDate(new Date(2019, 2, 26, 12, 0), { twentyFourHour: true })).toBe("26 Mar 2019, 12:00");
+      expect(Util.formatDate(new Date(2019, 2, 26, 18, 30), { twentyFourHour: true })).toBe("26 Mar 2019, 18:30");
+      expect(Util.formatDate(new Date(2019, 2, 26, 0, 0), { twentyFourHour: true })).toBe("26 Mar 2019, 0:00");
+    });
+  });
+
+  describe("secondsBetweenDates()", () => {
+    it("measures the time between two dates", () => {
+      expect.hasAssertions();
+
+      expect(Util.secondsBetweenDates(new Date(2018, 2, 26, 10, 30), new Date(2018, 2, 26, 10, 0))).toBe(-1800);
+      expect(Util.secondsBetweenDates(new Date(2000, 2, 26, 10, 30), new Date(2200, 2, 26, 10, 30))).toBe(6311347200);
+      expect(Util.secondsBetweenDates(new Date(2018, 2, 26, 10, 30), new Date(2018, 2, 26, 10, 30))).toBe(0);
+    });
+  });
+
+  describe("distanceOfTimeInWords()", () => {
+    it("outputs relative time in words", () => {
+      expect.hasAssertions();
+
+      expect(Util.distanceOfTimeInWords(1000, false)).toBe("17 minutes");
+      expect(Util.distanceOfTimeInWords(100000000, false)).toBe("3 years");
+      expect(Util.distanceOfTimeInWords(100000000, false, () => "3 blah")).toBe("3 blah");
+    })
+  })
+});
